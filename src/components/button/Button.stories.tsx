96d9c47e211af936d5806916761ba7942f7eb747
Andreas Hammer Håversen <andreas.h.haaversen@gmail.com>
2020-09-10 17:19:06 +0200

    Completed button component

---
+++ src/components/button/Button.stories.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { Story, Meta } from '@storybook/react'
+
+import { Button, ButtonProps } from '.'
+
+export default {
+  title: 'Button',
+  component: Button,
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+} as Meta
+
+const Template: Story<ButtonProps> = (args) => <Button {...args} />
+
+export const Standard = Template.bind({})
+Standard.args = {
+  label: 'Click me!',
+}
+
+export const Danger = Template.bind({})
+Danger.args = {
+  type: 'danger',
+  label: 'Click me!',
+  icon: 'delete',
+}
+
+export const Large = Template.bind({})
+Large.args = {
+  size: 'large',
+  label: 'Button',
+}
+
+export const Small = Template.bind({})
+Small.args = {
+  size: 'small',
+  label: 'Button',
+}
