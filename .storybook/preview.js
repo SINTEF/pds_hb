38fc0f1aebddecbae44680b53627768dca8324f6
Andreas Hammer Håversen <andreas.h.haaversen@gmail.com>
2020-09-15 13:59:23 +0200

    Setup backgrounds for Storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -2,4 +2,11 @@ import '../src/index.css'
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
+  backgrounds: {
+    default: 'grey',
+    values: [
+      { name: 'white', value: '#FFFFFF' },
+      { name: 'grey', value: '#EEEEEE' },
+    ],
+  },
 }
