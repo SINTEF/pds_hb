f9ba41f2d95ea8eb01782930d93b9b7dd551f7c2
Andreas Hammer Håversen <andreas.h.haaversen@gmail.com>
2020-09-22 09:13:50 +0200

    Added button snapshot test

---
+++ src/components/button/Button.test.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import renderer from 'react-test-renderer'
+import { Button } from '.'
+
+it('renders without crashing', () => {
+  const tree = renderer.create(<Button />).toJSON()
+  expect(tree).toMatchSnapshot()
+})
