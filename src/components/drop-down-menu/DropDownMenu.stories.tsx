f1de2052463bcd0c3c8bceb0eec203f40ab47d75
andrealouisestanderen <andreastanderen@gmail.com>
2020-09-22 09:50:14 +0200

    Add dropdownmenu component built up by menubutton components

--- src/components/drop-down-menu/DropDownMenu.stories.tsx
+++ src/components/drop-down-menu/DropDownMenu.stories.tsx
@@ -2,20 +2,20 @@ import React from 'react'
 import { Story, Meta } from '@storybook/react'
 
 import { DropDownMenu, DropDownMenuProps } from '.'
-/*
+
 export default {
-    title: 'DropDownMenu',
-    component: DropDownMenu,
-    argTypes: {
-      backgroundColor: { control: 'color' },
-    },
-  } as Meta
+  title: 'DropDownMenu',
+  component: DropDownMenu,
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+} as Meta
 
 const Template: Story<DropDownMenuProps> = (args) => <DropDownMenu {...args} />
 
-export const User = Template.bind({})
-User.args = {
-  label: 'Click me!',
+export const CompanyUser = Template.bind({})
+CompanyUser.args = {
+  isCompanyUser: true,
+  username: 'Steve Jobs',
+  company: 'Equinor AS',
 }
-
-*/
