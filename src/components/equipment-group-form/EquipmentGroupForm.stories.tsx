ebce494028273932c9c86afbcd43c90c1f185d97
Andreas Hammer Håversen <andreas.h.haaversen@gmail.com>
2020-09-22 12:58:05 +0200

    Completed first pass on equipment group component

---
+++ src/components/equipment-group-form/EquipmentGroupForm.stories.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Story, Meta } from '@storybook/react'
+import { EquipmentGroupForm, EquipmentGroupFormProps } from '.'
+
+export default {
+  title: 'Equipment group form',
+  component: EquipmentGroupForm,
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+} as Meta
+
+const Template: Story<EquipmentGroupFormProps> = (args) => (
+  <EquipmentGroupForm {...args} />
+)
+
+export const Standard = Template.bind({})
+Standard.args = {
+  isOpen: true,
+  onSave: (formValue) => console.log(formValue),
+}
