bc597e7a8d046844c51549e7ce340a0718313403
Andreas Hammer Håversen <andreas.h.haaversen@gmail.com>
2020-09-24 15:41:43 +0200

    Changed over to use storyshots. This wil generate automatic snapshot tests for each story! 🚀

--- src/components/contact-form/ContactForm.test.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import renderer from 'react-test-renderer'
-import { ContactForm } from '.'
-
-it('renders without crashing', () => {
-  const tree = renderer.create(<ContactForm />).toJSON()
-  expect(tree).toMatchSnapshot()
-})
--- src/components/menu-button/MenuButton.test.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import renderer from 'react-test-renderer'
-import { MenuButton } from '.'
-
-it('renders without crashing', () => {
-  const tree = renderer.create(<MenuButton label="Test button" />).toJSON()
-  expect(tree).toMatchSnapshot()
-})
