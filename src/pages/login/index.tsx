58360c4a382c8cfab5f80974454e6eb00bea9098
Andreas Hammer Håversen <andreas.h.haaversen@gmail.com>
2020-09-29 14:26:08 +0200

    Completed first pass on login page

---
+++ src/pages/login/index.tsx
@@ -0,0 +1,59 @@
+import React, { useState } from 'react'
+
+import { InputField } from '../../components/input-field'
+import { Button } from '../../components/button'
+
+import styles from './Login.module.css'
+
+export interface LoginForm {
+  username: string
+  password: string
+}
+
+export const Login: React.FC = () => {
+  const [form, setForm] = useState<LoginForm>({
+    username: '',
+    password: '',
+  })
+
+  const [success, setSuccess] = useState<boolean>(false)
+
+  const login: () => void = () => {
+    setSuccess(true)
+  }
+
+  return (
+    <div className={styles.main}>
+      <h1>PDS Data Handbook</h1>
+      <div className={styles.inputGroup}>
+        <InputField
+          label={'Username'}
+          icon={'face'}
+          onValueChanged={(value) =>
+            setForm({ ...form, username: value as string })
+          }
+          success={success}
+        />
+
+        <InputField
+          label={'Password'}
+          type={'password'}
+          icon={'lock'}
+          onValueChanged={(value) =>
+            setForm({ ...form, password: value as string })
+          }
+          success={success}
+        />
+      </div>
+      <div className={styles.buttonGroup}>
+        <Button label={'Log in'} onClick={() => login()} />
+        <Button
+          label={'Register new user'}
+          onClick={() => {
+            return
+          }}
+        />
+      </div>
+    </div>
+  )
+}
