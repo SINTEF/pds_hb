3cf0e54fc9811c78be6bb7ab5a61cf89bdbf9e2c
Andreas Hammer Håversen <andreas.h.haaversen@gmail.com>
2020-10-07 10:34:02 +0200

    Added cursor pointer, aria labels and optional fontsize to icon button

--- src/components/icon-button/index.tsx
+++ src/components/icon-button/index.tsx
@@ -5,15 +5,23 @@ import Ripples from 'react-ripples'
 export interface IconButtonProps {
   icon: string
   onClick: () => void
+  fontSize?: string
 }
 
 export const IconButton: React.FC<IconButtonProps> = ({
   icon,
   onClick,
+  fontSize = '24px',
 }: IconButtonProps) => {
   return (
     <Ripples>
-      <i className="material-icons" onClick={onClick}>
+      <i
+        className="material-icons"
+        onClick={onClick}
+        style={{ cursor: 'pointer', fontSize }}
+        aria-role="button"
+        aria-label={icon}
+      >
         {icon}
       </i>
     </Ripples>
