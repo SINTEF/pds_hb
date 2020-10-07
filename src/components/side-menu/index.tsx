681f881748b76e2e4c5ec6fc2b24507a024fdd1f
Andreas Hammer Håversen <andreas.h.haaversen@gmail.com>
2020-10-07 09:37:25 +0200

    Refactored side menu to take its menubuttons as children

--- src/components/side-menu/index.tsx
+++ src/components/side-menu/index.tsx
@@ -1,27 +1,17 @@
 import React from 'react'
 import styles from './sideMenu.module.css'
-import { MenuButton } from '../menu-button'
 
 export interface SideMenuProps {
-  menu: Array<string>
-
-  alert?: number
+  children: JSX.Element | JSX.Element[]
 }
 
 export const SideMenu: React.FC<SideMenuProps> = ({
-  menu,
-  alert,
+  children,
 }: SideMenuProps) => {
   return (
     <div className={styles.container}>
       <div className={styles.home}></div>
-      {menu.map((btn) =>
-        btn === 'Approve new users' ? (
-          <MenuButton label={btn} alert={alert} key={btn}></MenuButton>
-        ) : (
-          <MenuButton label={btn} key={btn}></MenuButton>
-        )
-      )}
+      {children}
     </div>
   )
 }
