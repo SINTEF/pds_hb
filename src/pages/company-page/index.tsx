5910e2b3baebb853a78e267c716a43a387b72c47
Andreas Hammer Håversen <andreas.h.haaversen@gmail.com>
2020-10-14 12:55:21 +0200

    Got company page styled

--- src/pages/company-page/index.tsx
+++ src/pages/company-page/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import { Switch, Route, NavLink, useRouteMatch } from 'react-router-dom'
+import { Switch, Route, useRouteMatch, useHistory } from 'react-router-dom'
+import { MenuButton } from '../../components/menu-button'
 
 import { SideMenu } from '../../components/side-menu'
 import { COMPANY_SUB_ROUTES } from '../../routes/routes.constants'
@@ -7,36 +8,58 @@ import { CompanyUserPage } from '../company-user-page'
 import { ManageFacilitiesPage } from '../manage-facilities-page'
 import { ManageStaffmembersPage } from '../manage-staffmembers-page'
 
+import styles from './CompanyPage.module.css'
+
 // TO FIX: Needs styling for menu
 export const CompanyPage: React.FC = () => {
   const { url, path } = useRouteMatch()
+  const history = useHistory()
   return (
-    <div>
-      <SideMenu>
-        <>
-          <NavLink to={url + COMPANY_SUB_ROUTES.REG_DATA}>
-            Register data
-          </NavLink>
-          <NavLink to={url + COMPANY_SUB_ROUTES.USER}>User</NavLink>
-          <NavLink to={url + COMPANY_SUB_ROUTES.MANAGE_FAC}>
-            Manage facilities
-          </NavLink>
-          <NavLink to={url + COMPANY_SUB_ROUTES.MANAGE_STAFF}>
-            Manage staff
-          </NavLink>
-        </>
-      </SideMenu>
-      <Switch>
-        <Route path={path + COMPANY_SUB_ROUTES.USER}>
-          <CompanyUserPage />
-        </Route>
-        <Route path={path + COMPANY_SUB_ROUTES.MANAGE_FAC}>
-          <ManageFacilitiesPage />
-        </Route>
-        <Route path={path + COMPANY_SUB_ROUTES.MANAGE_STAFF}>
-          <ManageStaffmembersPage />
-        </Route>
-      </Switch>
+    <div className={styles.page}>
+      <div className={styles.menuWrapper}>
+        <div className={styles.menu}>
+          <SideMenu>
+            <>
+              <MenuButton
+                onClick={() => history.push(url + COMPANY_SUB_ROUTES.REG_DATA)}
+                label="Registered data"
+              />
+              <MenuButton
+                onClick={() => history.push(url + COMPANY_SUB_ROUTES.USER)}
+                label="User page"
+              />
+              <MenuButton
+                onClick={() =>
+                  history.push(url + COMPANY_SUB_ROUTES.MANAGE_FAC)
+                }
+                label="Manage Facilities"
+              />
+              <MenuButton
+                onClick={() =>
+                  history.push(url + COMPANY_SUB_ROUTES.MANAGE_STAFF)
+                }
+                label="Manage staff"
+              />
+            </>
+          </SideMenu>
+        </div>
+      </div>
+      <div className={styles.content}>
+        <Switch>
+          <Route path={path + COMPANY_SUB_ROUTES.REG_DATA}>
+            {/* <RegisteredData />  this component needs some work before it can be used*/}
+          </Route>
+          <Route path={path + COMPANY_SUB_ROUTES.USER}>
+            <CompanyUserPage />
+          </Route>
+          <Route path={path + COMPANY_SUB_ROUTES.MANAGE_FAC}>
+            <ManageFacilitiesPage />
+          </Route>
+          <Route path={path + COMPANY_SUB_ROUTES.MANAGE_STAFF}>
+            <ManageStaffmembersPage />
+          </Route>
+        </Switch>
+      </div>
     </div>
   )
 }
