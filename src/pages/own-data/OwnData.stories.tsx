a7ff71ee3c01ff9ce951bf2f628edf982af126be
Håvard_Lund <haavalu@stud.ntnu.no>
2020-10-28 12:26:43 +0100

    adds filters to get to browse own equipment page

---
+++ src/pages/own-data/OwnData.stories.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Story, Meta } from '@storybook/react'
+
+import { OwnDataPage } from '.'
+
+export default {
+  title: 'pages/ChooseComponentPagePage',
+  component: OwnDataPage,
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+} as Meta
+
+const Template: Story = (args) => <OwnDataPage {...args} />
+
+export const Standard = Template.bind({})
